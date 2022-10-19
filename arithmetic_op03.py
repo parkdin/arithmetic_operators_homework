0bd4a4feab0de791b161b7686293fc13c35a1d05
parkdin <abdukokhorovizatillo00@gmail.com>
2022-10-19 23:43:01 +0900

    03 done

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 25
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer = number%10*10+number//10
 #Print the answer variable
+print(answer)
